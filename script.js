0a12ae7744b5b526edaa3dc33fd1d563c5a52404
Dimitar Vashkov <vashkov.dimitar@gmail.com>
2018-03-18 18:38:17 +0000

    Add factory for player and board

--- script.js
+++ script.js
@@ -3,3 +3,27 @@
 // Create modules for board and game
 
 // Separate render and game logic
+'use strict';
+
+const easyAI = document.querySelector('#easyAI');
+const hardAI = document.querySelector('#hardAI');
+const multiplayer = document.querySelector('#multiplayer');
+const options = document.querySelector('#options');
+
+
+// Factory for player
+const playerFactory = (name, symbol) => {
+  const info = () => console.log(name, symbol);
+  return { info, name, symbol };
+};
+
+// Factory for board
+const boardFactory = () => {
+  let board = ['', '', '', '', '', '', '', '', ''];
+  const update = (index, symbol) => board[index] = symbol;
+  return { board, update };
+};
+
+easyAI.addEventListener('click', function () {
+  options.classList.add('hidden');
+});
